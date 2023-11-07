c2c253694c17485c4f25751a10ed085610b7477a
Carol <carol.leiva@tsoftglobal.com>
2023-11-07 09:19:23 -0300

    Initial commit

---
+++ src/main/java/IfAndElse.java
@@ -0,0 +1,10 @@
+public class If {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+
+        if (num2 > num1) {
+            System.out.println("El num2 es mayor al num1");
+        }
+    }
+}
---
+++ src/main/java/RepetitivasMatrices.java
@@ -0,0 +1,16 @@
+public class RepetitivasMatrises {
+    public static void main(String[] args) {
+        //Declaraciones
+        //las Matrices son como los cajitas montadas en un pallet o como un cubo rubic
+
+        int matriz [][] = new int [3][3];
+
+        //Asignacion (Manual)
+
+        matriz[0][0] = 5;
+        matriz[0][1] = 13;
+        matriz[2][2] = 24;
+
+
+    }
+}
